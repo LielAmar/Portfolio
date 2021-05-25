1079e23dc8637c2bb1ca5bf6b089b7c1d70a47d6
Liel <lialiko3@gmail.com>
2021-05-25 18:47:06 +0300

    Google Analytics Support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
+import { useEffect } from 'react';
+
 import "../styles/global.css";
 
 const App = ({ Component, pageProps }: any) => {
+
+  // Loading google analytics
+  useEffect(() => {
+    const s = document.createElement("script");
+    s.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-7VB04G99YZ");
+    s.setAttribute("async", "true");
+    document.head.appendChild(s);
+
+    (window as any).dataLayer = (window as any).dataLayer || [];
+
+    function gtag(...args: any[]) {
+      (window as any).dataLayer.push(args);
+    }
+
+    gtag('js', new Date());
+    gtag('config', 'G-7VB04G99YZ');
+  }, []);
+
   return <Component {...pageProps} />
 }
 
