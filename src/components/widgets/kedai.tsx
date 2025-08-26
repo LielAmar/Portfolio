ab69c1e1104d97efceed13add9af3232629c5d2d
Liel Amar <liel@lielamar.com>
2025-08-26 11:04:07 +0300

    Fix agent id

--- src/components/widgets/kedai.tsx
+++ src/components/widgets/kedai.tsx
@@ -7,7 +7,7 @@ const KedAIWidget = () => {
     const script = window.document.createElement("script");
     script.type = "text/javascript";
     script.src = "https://cdn.kedai.co.il/kedai-widget.js";
-    script.setAttribute("agent-id", "xTvQH52f0IgEzndao5EH");
+    script.setAttribute("agent-id", "78cKKtLidSHg1ZqMlGY0");
     script.setAttribute("language", "en");
     window.document.getElementsByTagName("head")[0].appendChild(script);
   };
